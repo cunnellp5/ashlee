434b24feaf68734ca9fed5b1928b3bd71aefc12e
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-01 13:16:18 -0400

    messing with parallax

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1 class="text-xs-center">{{ msg }}</h1>
     <div class="mi">
-      <v-parallax src="http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c0.112.899.899/16230558_153781181794043_7338875721789472768_n.jpg?ig_cache_key=MTQ0NTUyNDIzODEyMjY4NjMyMw%3D%3D.2.c"></v-parallax>
+      <v-parallax height="400" src="https://i.ytimg.com/vi/8QMWkAn35LQ/maxresdefault.jpg"></v-parallax>
     </div>
   </div>
 </template>
@@ -25,6 +25,7 @@ export default {
     /*display: flex;
     justify-content: center;*/
   }
+
 
 
 </style>
