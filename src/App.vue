18ccc458ddf17b65a7f2cb47e9be5c9185cef0df
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-01 12:49:48 -0400

    started fan page for friend

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app id="app">
+    <v-toolbar>
+      <v-toolbar-title></v-toolbar-title>
+    </v-toolbar>
+      <main>
+        <v-content>
+          <v-container fluid>
+            <router-view></router-view>
+          </v-container>
+        </v-content>
+      </main>
+    <v-footer></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style scoped>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+  #app {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1 class="text-xs-center">{{ msg }}</h1>
+    <div class="mi">
+      <v-parallax src="http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c0.112.899.899/16230558_153781181794043_7338875721789472768_n.jpg?ig_cache_key=MTQ0NTUyNDIzODEyMjY4NjMyMw%3D%3D.2.c"></v-parallax>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Sm-Ashlee',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .mi {
+    height: 1000px;
+    /*display: flex;
+    justify-content: center;*/
+  }
+
+
+</style>
